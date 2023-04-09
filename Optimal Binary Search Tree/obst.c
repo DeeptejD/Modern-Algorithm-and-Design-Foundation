fc43151cb284a65b75e61ff45243df179658d9f9
DeeptejD <deeptejdhauskar2003@gmail.com>
2023-04-09 21:13:37 +0530

    OBST with numbers (string to be implemented)

--- Optimal Binary Search Tree/obst.c
+++ Optimal Binary Search Tree/obst.c
@@ -1,8 +1,79 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <limits.h>
 #define sint(x) scanf("%d", &x);
+#define N 20
+#define inf INT_MAX
+
+struct key
+{
+    int w, r, c;
+};
+
+void get0(int a[N], int n)
+{
+    for (int i = 0; i <= n; i++)
+        sint(a[i]);
+}
+
+void get1(int a[N], int n)
+{
+    for (int i = 1; i <= n; i++)
+        sint(a[i]);
+}
+
+int find(struct key g[][N], int i, int j)
+{
+    int min = inf, l;
+    for (int m = g[i][j - 1].r; m <= g[i + 1][j].r; m++)
+    {
+        if ((g[i][m - 1].c + g[m][j].c) < min)
+        {
+            min = g[i][m - 1].c + g[m][j].c;
+            l = m;
+        }
+    }
+    return l;
+}
+
+void obst(struct key g[][N], int q[N], int p[N], int n)
+{
+    int k;
+    for (int i = 0; i < n - 1; i++)
+    {
+        // init
+        g[i][i].w = q[i], g[i][i].r = g[i][i].c = 0;
+        g[i][i + 1].w = q[i] + q[i + 1] + p[i + 1];
+        g[i][i + 1].r = i + 1;
+        g[i][i + 1].c = q[i] + q[i + 1] + p[i + 1];
+    }
+    g[n][n].w = q[n], g[n][n].r = g[n][n].c = 0;
+    for (int m = 2; m <= n; m++)
+    {
+        for (int i = 0; i <= n - m; i++)
+        {
+            int j = i + m;
+            g[i][j].w = g[i][j - 1].w + p[j] + q[j];
+            k = find(g, i, j);
+            g[i][j].c = g[i][j].w + g[i][k - 1].c + g[k][j].c;
+            g[i][j].r = k;
+        }
+    }
+    printf("c[%d][%d] = %d\nw[%d][%d] = %d\nr[%d][%d] = %d\n", 0, n, g[0][n].c, 0, n, g[0][n].w, 0, n, g[0][n].r);
+}
 
 int main(int argc, char const *argv[])
 {
-    
+    int n;
+    int a[N], p[N], q[N];
+    struct key g[N][N];
+    printf("Enter the number of identifiers: ");
+    sint(n);
+    printf("Enter the identifiers: ");
+    get1(a, n);
+    printf("Enter the probabilities for a succesfull search: ");
+    get1(p, n);
+    printf("Enter the probablities for an unsuccesfull search: ");
+    get0(q, n);
+    obst(g, q, p, n);
     return 0;
 }
