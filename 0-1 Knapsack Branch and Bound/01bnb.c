b823837fde3948a14c448a6ca5e78affb13a1d6c
DeeptejD <deeptejdhauskar2003@gmail.com>
2023-05-31 19:02:36 +0530

    Added 0/1 Knapsakc usinng Branch and Bound

--- 0-1 Knapsack Branch and Bound/01bnb.c
+++ 0-1 Knapsack Branch and Bound/01bnb.c
@@ -1,123 +1,172 @@
 #include <stdio.h>
+#include <limits.h>
+#include <math.h>
 #define sint(x) scanf("%d", &x)
-#define N 100
+#define N 50
 
-struct ITEM
+struct item
 {
-    int w, p;
+    int p, w;
+    float ratio;
 };
 
-int m, n, currentsol[N], bestsol[N], cw, cp, bestp;
-struct ITEM items[N];
+int m, n, upper = INT_MAX, maxp, maxw, y[N], x[N], z[N];
 
-void fillz()
+void _sort(struct item k[])
 {
-    for (int i = 0; i < n; i++)
-        currentsol[i] = bestsol[i] = 0;
-}
-
-int ub(int level, int w, int p)
-{
-    int r = m - w; // remaining knapsack cap
-    int upperbound = p;
-
-    for (int i = level; i < n; i++)
-    {
-        if (items[i].w <= r)
-        {
-            r -= items[i].w;
-            upperbound += items[i].p;
-        }
-        else
-        {
-            upperbound += (r / items[i].w) * items[i].p; // fractional weight
-            break;
-        }
-    }
-    return upperbound;
-}
-
-void _sort()
-{
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n - 1; i++)
     {
         for (int j = 0; j < n - i - 1; j++)
         {
-            if ((items[j].p / items[j].w) < (items[j + 1].p / items[j + 1].w))
+            if (k[j].ratio < k[j + 1].ratio)
             {
-                int tempp = items[j].p, tempw = items[j].w;
-                items[j].p = items[j + 1].p, items[j].w = items[j + 1].w;
-                items[j + 1].p = tempp, items[j + 1].w = tempw;
+                struct item temp = k[j];
+                k[j] = k[j + 1];
+                k[j + 1] = temp;
             }
         }
     }
 }
 
-void bnb(int level, int w, int p)
+int bound(int curr_index, int cp, int cw, struct item k[])
 {
-    if (w <= m) // current weight is less than the knapsack cap
+    // calculates the fractional upper bound
+    int b = cp, c = cw;
+    for (int i = curr_index; i < n; i++)
     {
-        if (p > bestp)
+        c += k[i].w;
+        if (c < m)
+            b += k[i].p;
+        else
+            return (-1) * (b + (int)ceil((1 - (c - m) / (float)k[i].w) * k[i].p));
+    }
+    return -b;
+}
+
+int ubound(int curr_index, int cp, int cw, struct item k[])
+{
+    int b = cp, c = cw;
+    for (int i = curr_index; i < n; i++)
+    {
+        if (c + k[i].w <= m)
         {
-            bestp = p;
-            for (int i = 0; i < n; i++)
-                bestsol[i] = currentsol[i];
+            c += k[i].w;
+            b += k[i].p;
         }
     }
+    return -b;
+}
 
-    if (level == n || ub(level, w, p) <= bestp) // if all the items are exhausted or the upper bound of the subtree is less than the current best p
-        return;
+void bnb(int curr_index, int cp, int cw, struct item k[])
+{
+    // calc fractional bound
+    int c = bound(curr_index, cp, cw, k);
+    // calc non fractional bound
+    int u = ubound(curr_index, cp, cw, k);
 
-    currentsol[level] = 1;
-    bnb(level + 1, w + items[level].w, p + items[level].p); // including the item at the current level
+    if (u < upper && cw <= m)
+        upper = u;
 
-    currentsol[level] = 0;
-    bnb(level + 1, w, p); // exclusing the item at the current level
+    // left child
+    if (c <= upper)
+    {
+        y[curr_index] = 1;      // included current element in the sol
+        if (curr_index < n - 1) // more items to consider
+            if (cw <= m)
+                bnb(curr_index + 1, cp + k[curr_index].p, cw + k[curr_index].w, k);
+
+        // last node
+        if (curr_index == n - 1)
+        {
+            if (cw + k[curr_index].w <= m)
+            {
+                maxp = cp + k[curr_index].p, maxw = cw + k[curr_index].w;
+
+                for (int j = 0; j < curr_index; j++)
+                    x[j] = y[j];
+                x[curr_index] = 1;
+            }
+        }
+    }
+    // right child
+    if (c <= upper)
+    {
+        y[curr_index] = 0;      // excluding the current element
+        if (curr_index < n - 1) // more items left to explore
+            bnb(curr_index + 1, cp, cw, k);
+
+        if (curr_index == n - 1) // last item
+        {
+            int c = bound(curr_index, cp - k[curr_index].p, cw - k[curr_index].w, k);
+            int b = ubound(curr_index, cp - k[curr_index].p, cw - k[curr_index].w, k);
+            if (cw <= m && c <= upper)
+            {
+                maxp = cp, maxw = cw;
+                for (int j = 0; j < curr_index; j++)
+                    x[j] = y[j];
+                x[curr_index] = 0;
+            }
+        }
+    }
 }
 
 int main(int argc, char const *argv[])
 {
-    printf("Enter knapsack cap: "), sint(m);
-    printf("Enter the number of items: "), sint(n);
-    printf("Enter the weight and profit for each item\n");
+    printf("Number of items: "), sint(n), printf("Max cap: "), sint(m);
+    struct item knapsack[n + 1];
+    int p[N], w[N];
+    printf("Enter Profits and Weights\n");
     for (int i = 0; i < n; i++)
-        sint(items[i].p), sint(items[i].w);
-    cw = cp = bestp = 0;
-    fillz();
-    _sort();
-    bnb(0, cw, cp);
+        sint(p[i]), sint(w[i]);
+    // init_knapsack(p, w, knapsack);
+    for (int i = 0; i < n; i++)
+        knapsack[i].p = p[i], knapsack[i].w = w[i], knapsack[i].ratio = (float)(knapsack[i].p / knapsack[i].w);
+    _sort(knapsack);
+    maxp = -1;
+    bnb(0, 0, 0, knapsack);
 
-    printf("Knapsack Solution\nMax Profit = %d\nItems Included: ", bestp);
     for (int i = 0; i < n; i++)
-        printf(" %d", bestsol[i]);
+        for (int j = 0; j < n; j++)
+            if (p[i] == knapsack[j].p && w[i] == knapsack[j].w)
+            {
+                if (x[j])
+                    z[i] = 1;
+                else
+                    z[i] = 0;
+            }
+
+    printf("Solution\n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", z[i]);
     printf("\n");
-
+    printf("Max profit: %d\nMax weight: %d\n", maxp, maxw);
     return 0;
 }
 
 /*
-Test case
+Testcase
 
-35
 4
+35
 80 14
 82 17
-42 13
 45 15
+42 13
 
 */
 
 /*
 Output
 
-Enter knapsack cap: 35
-Enter the number of items: 4
-Enter the weight and profit for each item
+Number of items: 4
+Max cap: 35
+Enter Profits and Weights
 80 14
 82 17
-42 13
 45 15
-Knapsack Solution
-Max Profit = 162
-Items Included:  1 1 0 0
+42 13
+Solution
+1 1 0 0
+Max profit: 162
+Max weight: 31
 */
